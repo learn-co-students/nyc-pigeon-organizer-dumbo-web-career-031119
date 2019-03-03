e84ceb445ce6d03f0a9d577fe6c9708dacd13092
siberry <siberry226@gmail.com>
2019-03-02 22:41:49 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data[:gender].each do |x,y|
+    y.each do |z|
+      pigeon_list[z] = {:gender => [x.to_s]}
+      data.each do |k,v|
+        v.each do |k2, v2|
+          if !pigeon_list[z][k].is_a?(Array) && v2.include?(z)
+            pigeon_list[z][k] = [k2.to_s]
+          elsif k == :color && v2.include?(z)
+            pigeon_list[z][k] << k2.to_s
+          end
+        end
+      end
+    end
+  end
+  pigeon_list
 end
+
+#x is male or female; y is array of pigeons of gender x
+#z is pigeon name
+#k is attribute, v is hash of description w/ array of pigeons
+#k2 is description, v2 is array of pigeons
