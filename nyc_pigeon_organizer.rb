0d496410c35b42aaf6e934db848dde94e6cde7d9
John Pena <jpena91@gmail.com>
2019-03-09 17:15:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+   new_hash = {}
+
+   data.each do |colors_gender, options|
+             #colors_gender = :color, :gender, : lives
+             #options = {:purple => [Array of Names]}
+     options.each do |option, pigeons|
+             #options = :purple => ["Theo", "Peter"..]
+              #pigeons = [:color] ["Theo", "Peter", "Lucky"]
+       pigeons.each do |pigeon|
+         new_hash[pigeon] ||= {}
+         new_hash[pigeon][colors_gender] ||= []
+        new_hash[pigeon][colors_gender] << option.to_s
+      end
+     end
+   end
+   new_hash
+ end 
